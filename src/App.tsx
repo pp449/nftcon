381f2f183bacc7ff9c0c985c986805ac2d22babe
Lee sang Yeop <mma7710@naver.com>
2023-09-07 18:48:23 +0900

    Fix: Footer 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import React from "react"
-import { Route, Routes } from "react-router-dom"
-import MainContainer from "./components/MainContainer";
-import AuthTokenForm from "./components/AuthTokenForm"
-import Navbar from "./components/Navbar"
-import HomePage from "./pages/HomePage"
-import ItemInfoPage from "./pages/ItemInfoPage"
-import MintPage from "./pages/MintPage"
-import MyPage from "./pages/MyPage"
-import Footer from "./components/Footer";
+import { Route, Routes } from 'react-router-dom';
+import MainContainer from './components/MainContainer';
+import AuthTokenForm from './components/AuthTokenForm';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import ItemInfoPage from './pages/ItemInfoPage';
+import MintPage from './pages/MintPage';
+import MyPage from './pages/MyPage';
 
 function App() {
-	
-
-  return (
+	return (
 		<>
 			<Navbar />
 			<MainContainer>
@@ -26,9 +22,8 @@ function App() {
 					</Route>
 				</Routes>
 			</MainContainer>
-			<Footer />
 		</>
 	);
 }
 
-export default App
+export default App;
