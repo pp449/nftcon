0a0bd2c9e91a2fc14db5cf7f1406f1ec63abe1d0
mma7710 <mma7710@naver.com>
2023-09-07 16:50:17 +0000

    Config: 배포 되도록 vite config 설정

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ WORKDIR /usr/src/app
 COPY . .
 RUN npm install
 EXPOSE 3000
-CMD ["npm", "run", "dev", "--host", "0.0.0.0" "--port", "3000"]
+CMD ["npm", "run", "dev", "--host", "0.0.0.0", "--port", "3000"]
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const env = loadEnv(process.cwd(), '')
+const staticServerUri = env.VITE_A || "";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: `/${staticServerUri}`,
   plugins: [react()]
 })
 
