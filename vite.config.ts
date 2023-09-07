8d3d2bad9076a29faa69842c792c1d129f97381f
mma7710 <mma7710@naver.com>
2023-09-07 17:23:01 +0000

    ll

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const env = loadEnv(process.cwd(), '')
-const staticServerUri = env.VITE_A || "";
+const staticServerUri = "k3d5b0c62ef97a";
 
 // https://vitejs.dev/config/
 export default defineConfig({
